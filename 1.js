5d9f36770ba4cbb162c1a3cb80b5ff5434df880c
tgispert <spfury@gmail.com>
2016-11-09 17:13:12 -0300

    enemigo avanza y mejor manejo de document.get

--- 1.js
+++ 1.js
@@ -4,31 +4,41 @@ const S_KEY = 115;
 const D_KEY = 100;
 const RUNNING = "running";
 const PAUSED = "paused";
+var pace = 10;
+var ratio = 1;
+var puntos = 0;
+var enemigo = 1000;
+var capa1 = document.getElementById("capa1");
+var capa2 = document.getElementById("capa2");
+var capa3 = document.getElementById("capa3");
+var personaje = document.getElementById("personaje");
 
 function estadoPaisaje(e){
-  // var aux = document.getElementsByClassName("capa1");
-  // for (var i = 0; i < aux.length; i++) {
-  //   aux[i].style.animationPlayState = e;
-  // }
-  document.getElementById("capa1").style.animationPlayState = e;
-  document.getElementById("capa2").style.animationPlayState = e;
-  document.getElementById("capa3").style.animationPlayState = e;
+  capa1.style.animationPlayState = e;
+  capa2.style.animationPlayState = e;
+  capa3.style.animationPlayState = e;
+  if(e == RUNNING){
+    pace = 15*ratio;
+  }
+  else {
+    pace = 10*ratio;
+  }
 }
 
 function walk(){
-  document.getElementById("personaje").style.animation = "walk 0.7s steps(9, end) infinite";
+  personaje.style.animation = "walk 0.7s steps(9, end) infinite";
 }
 
 function jump(){
-  document.getElementById("personaje").style.animation = "jump 0.7s steps(10, end)";
+  personaje.style.animation = "jump 0.7s steps(10, end)";
 }
 
 function idle(){
-  document.getElementById("personaje").style.animation = "idle 3s steps(10, end) infinite";
+  personaje.style.animation = "idle 3s steps(10, end) infinite";
 }
 
 function headbutt(){
-  document.getElementById("personaje").style.animation = "headbutt 0.7s steps(9, end)";
+  personaje.style.animation = "headbutt 0.7s steps(9, end)";
 }
 
 document.onkeypress = function(e){
@@ -56,3 +66,18 @@ document.onkeypress = function(e){
       idle();
   }
 }
+
+function avanzarEnemigos() {
+  var aux = document.getElementsByClassName("enemigo");
+  for (var i = 0; i < aux.length; i++) {
+     enemigo = enemigo - pace;
+     aux[i].style.left = enemigo +"px";
+  }
+  puntos = setTimeout(avanzarEnemigos, 40);
+}
+
+function playGame(){
+  avanzarEnemigos();
+}
+
+window.onload = playGame;
